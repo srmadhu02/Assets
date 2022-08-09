efeefbcde5ec53d4d5bbf0e4e86c91285e1beb59
Madhu <srmadhu02@gmail.com>
2022-08-09 19:13:24 +0530

    Add existing project files to Git

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms_sqlalchemy.fields import QuerySelectField
 #import random
 
 app=Flask(__name__)
@@ -8,14 +10,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
 class Asset(db.Model):
-    Name=db.Column(db.String(length=30),nullable=False)
+    Name=db.Column(db.String(length=30))
     Asset_ID=db.Column(db.Integer(),primary_key=True)
-    Classification=db.Column(db.String(length=30),nullable=False)
-    Category=db.Column(db.String(length=12),nullable=False)
+    Classification=db.Column(db.String(length=30))
+    Type=db.Column(db.String(length=30))
+    Category=db.Column(db.String(length=30))
+    
 
     def __repr__(self) ->str:
         return f'Asset {self.Name} - {self.Asset_ID}'
 
+
+ 
 @app.route("/home")
 @app.route("/")
 def home():
@@ -28,11 +34,25 @@ def assets():
         Name=request.form['Name']
         Classification=request.form['Classification']
         Category=request.form['Category']
+        Type=request.form['Type']
+        Asset_ID=request.form.get('Asset_ID')
         
-        asset=Asset(Name=Name,Classification=Classification,Category=Category)
+        '''if Category is not None :       
+            workout = Asset(Category=Category)
+            db.session.add(workout)
+            db.session.commit() 
+            # you can redirect to home page on successful commit. or anywhere else
+            return redirect(url_for('assets'))       
+        else:
+            error="Error, your log is incomplete! Please check and submit it again!"'''
+
+
+        
+        asset=Asset(Name=Name,Asset_ID=Asset_ID,Classification=Classification,Category=Category,Type=Type)
         db.session.add(asset)
         db.session.commit()
     assets=Asset.query.all()
+    
     return render_template("assets.html",assets=assets)
 
 @app.route("/newAsset", methods=['GET','POST'])
@@ -42,14 +62,17 @@ def new_asset():
         Name=request.form['Name']
         Classification=request.form['Classification']
         Category=request.form['Category']
+        Type=request.form['Type']
         
-        asset=Asset(Name=Name,Classification=Classification,Category=Category)
+        asset=Asset(Name=Name,Classification=Classification,Category=Category,Type=Type)
         db.session.add(asset)
         db.session.commit()
+    
         
-        
+    Types = ['Laptop', 'Mobile', 'Others']
     allAssets=Asset.query.all()
-    return render_template("add_asset.html",assets=assets)
+    
+    return render_template("add_asset.html",assets=assets,Types=Types)
 
 @app.route("/update/<int:Asset_ID>",methods=['GET','POST'])
 def update(Asset_ID):
@@ -58,16 +81,18 @@ def update(Asset_ID):
         Name=request.form['Name']
         Classification=request.form['Classification']
         Category=request.form['Category']
+        Type=request.form['Type']
         
         asset.Name=Name
         asset.Classification=Classification
         asset.Category=Category
+        asset.Type=Type
         db.session.add(asset)
         db.session.commit()
         return redirect("/assets")
-    
+    Types = ['Laptop', 'Mobile', 'Others']
     asset=Asset.query.filter_by(Asset_ID=Asset_ID).first()
-    return render_template('update.html',asset=asset)
+    return render_template('update.html',asset=asset,Types=Types)
 
 @app.route("/delete/<int:Asset_ID>")
 def delete(Asset_ID):
